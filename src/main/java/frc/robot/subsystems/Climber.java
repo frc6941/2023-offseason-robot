18226b76bf54d3e03c7eb6f57ca317281c50a99a
WilliamLi0623 <61577379+WilliamLi0623@users.noreply.github.com>
2023-07-20 16:09:19 +0800

    Editing Climber Code (UNFINISHED)

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -17,109 +17,133 @@ import org.frcteam6941.utils.CTREFactory;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants.ClimberConstants;
+import frc.robot.subsystems.Hood.STATE;
 import frc.robot.Ports;
 
 /** Add your docs here. */
-public class Climber implements Updatable, Subsystem{
-        public static class PeriodicIO{
-            //INPUT
-            double hookVoltage;
-            double hookCurrent;
-            double hookPosition;
-            double hookVelocity;
+public class Climber implements Updatable, Subsystem {
+    public static class PeriodicIO {
+        // INPUT
+        public double hookVoltage;
+        public double hookCurrent;
+        public double hookPosition;
+        public double hookVelocity;
 
-            double pusherVoltage;
-            double pusherCurrent;
-            double pusherPosition;
-            double pusherVelocity;
+        public double pusherVoltage;
+        public double pusherCurrent;
+        public double pusherPosition;
+        public double pusherVelocity;
 
-            //OUTPUT
-            double hookDemand;
+        // OUTPUT
+        public double hookDemand;
 
-            double pusherDemand;
-        }
+        public double pusherDemand;
+    }
 
-        public PeriodicIO periodicIO = new PeriodicIO();
+    public PeriodicIO periodicIO = new PeriodicIO();
 
-        public TalonFX hookMotor = CTREFactory.createDefaultTalonFX(Ports.CanId.Canivore.CLIMBER_HOOK, false);
-        public TalonFX pusherMotor = CTREFactory.createDefaultTalonFX(Ports.CanId.Canivore.CLIMBER_PUSHER, false);
+    public TalonFX hookMotor = CTREFactory.createDefaultTalonFX(Ports.CanId.Canivore.CLIMBER_HOOK, false);
+    public TalonFX pusherMotor = CTREFactory.createDefaultTalonFX(Ports.CanId.Canivore.CLIMBER_PUSHER, false);
 
-        //private boolean isCalibrated = false;
+    // private boolean isCalibrated = false;
 
-        public STATE state = STATE.LOCKED;
-        public enum STATE{
-            HOMING,
-            ANGLE,
-            PERCENTAGE,
-            LOCKED
-        }
+    public STATE state = STATE.LOCKED;
 
-        private static Climber instance;
+    public enum STATE {
+        HOMING,
+        ANGLE,
+        PERCENTAGE,
+        LOCKED
+    }
 
-        private Climber(){
-            hookMotor.configFactoryDefault();
-            hookMotor.setNeutralMode(NeutralMode.Brake);
-            hookMotor.config_kP(0, ClimberConstants.HOOK_KP.get());
-            hookMotor.config_kI(0, ClimberConstants.HOOK_KI.get());
-            hookMotor.config_kD(0, ClimberConstants.HOOK_KD.get());
-            hookMotor.config_kF(0, ClimberConstants.HOOK_KF.get());
-            hookMotor.configMotionCruiseVelocity(ClimberConstants.HOOK_CRUISE_V);
-            hookMotor.configMotionAcceleration(ClimberConstants.HOOK_CRUISE_ACC);
-            hookMotor.enableVoltageCompensation(true);
-            hookMotor.config_IntegralZone(0, 50);
-            hookMotor.configNeutralDeadband(0.01);
-            hookMotor.configMotionSCurveStrength(ClimberConstants.HOOK_S_STRENGTH);
+    private static Climber instance;
 
-            pusherMotor.configFactoryDefault();
-            pusherMotor.setNeutralMode(NeutralMode.Brake);
-            pusherMotor.config_kP(0, ClimberConstants.PUSHER_KP.get());
-            pusherMotor.config_kI(0, ClimberConstants.PUSHER_KI.get());
-            pusherMotor.config_kD(0, ClimberConstants.PUSHER_KD.get());
-            pusherMotor.config_kF(0, ClimberConstants.PUSHER_KF.get());
-            pusherMotor.configMotionCruiseVelocity(ClimberConstants.PUSHER_CRUISE_V);
-            pusherMotor.configMotionAcceleration(ClimberConstants.PUSHER_CRUISE_ACC);
-            pusherMotor.enableVoltageCompensation(true);
-            pusherMotor.config_IntegralZone(0, 50);
-            pusherMotor.configNeutralDeadband(0.01);
-            pusherMotor.configMotionSCurveStrength(ClimberConstants.PUSHER_S_STRENGTH);
+    private Climber() {
+        hookMotor.configFactoryDefault();
+        hookMotor.setNeutralMode(NeutralMode.Brake);
+        hookMotor.config_kP(0, ClimberConstants.HOOK_KP.get());
+        hookMotor.config_kI(0, ClimberConstants.HOOK_KI.get());
+        hookMotor.config_kD(0, ClimberConstants.HOOK_KD.get());
+        hookMotor.config_kF(0, ClimberConstants.HOOK_KF.get());
+        hookMotor.configMotionCruiseVelocity(ClimberConstants.HOOK_CRUISE_V);
+        hookMotor.configMotionAcceleration(ClimberConstants.HOOK_CRUISE_ACC);
+        hookMotor.enableVoltageCompensation(true);
+        hookMotor.config_IntegralZone(0, 50);
+        hookMotor.configNeutralDeadband(0.01);
+        hookMotor.configMotionSCurveStrength(ClimberConstants.HOOK_S_STRENGTH);
 
-        }
+        pusherMotor.configFactoryDefault();
+        pusherMotor.setNeutralMode(NeutralMode.Brake);
+        pusherMotor.config_kP(0, ClimberConstants.PUSHER_KP.get());
+        pusherMotor.config_kI(0, ClimberConstants.PUSHER_KI.get());
+        pusherMotor.config_kD(0, ClimberConstants.PUSHER_KD.get());
+        pusherMotor.config_kF(0, ClimberConstants.PUSHER_KF.get());
+        pusherMotor.configMotionCruiseVelocity(ClimberConstants.PUSHER_CRUISE_V);
+        pusherMotor.configMotionAcceleration(ClimberConstants.PUSHER_CRUISE_ACC);
+        pusherMotor.enableVoltageCompensation(true);
+        pusherMotor.config_IntegralZone(0, 50);
+        pusherMotor.configNeutralDeadband(0.01);
+        pusherMotor.configMotionSCurveStrength(ClimberConstants.PUSHER_S_STRENGTH);
 
+    }
 
+    public synchronized void ResetHook(double angle) {
+        hookMotor.setSelectedSensorPosition(
+                Conversions.degreesToFalcon(angle, ClimberConstants.HOOK_GEAR_RATIO));
+    }
 
-        public synchronized void ResetHook(double angle){
-            hookMotor.setSelectedSensorPosition(
-                Conversions.degreesToFalcon(angle, ClimberConstants.HOOK_GEAR_RATIO)
-            );
-
-        }
-
-        public synchronized void SetHookAngle(double angle){
+    public synchronized void SetHookAngle(double angle) {
+        if (getState() != STATE.ANGLE) {
             setState(STATE.ANGLE);
-            angle = Util.clamp(
-                angle, ClimberConstants.HOOK_MIN_ANGLE, ClimberConstants.HOOK_MAX_ANGLE
-                );
-            periodicIO.hookDemand = angle;
         }
+        angle = Util.clamp(
+                angle, ClimberConstants.HOOK_MIN_ANGLE, ClimberConstants.HOOK_MAX_ANGLE);
+        periodicIO.hookDemand = angle;
+    }
 
-        public synchronized void SetHookPercentage(double power){
+    public synchronized void SetHookPercentage(double power) {
+        if (getState() != STATE.PERCENTAGE) {
             setState(STATE.PERCENTAGE);
-            periodicIO.hookDemand = power;
         }
-        
+        periodicIO.hookDemand = power;
+    }
 
-        public static Climber getInstance(){
-            if(instance==null){
-                instance = new Climber();
-            }
-            return instance;
+    public static Climber getInstance() {
+        if (instance == null) {
+            instance = new Climber();
         }
+        return instance;
+    }
 
-        public void setState(STATE state) {
-            this.state = state;
+    public synchronized void setHookMinimum() {
+        SetHookAngle(ClimberConstants.HOOK_MIN_ANGLE);
+    }
+
+    public synchronized double getHookAngle() {
+        return Conversions.falconToDegrees(periodicIO.hookPosition, ClimberConstants.HOOK_GEAR_RATIO)
+    }
+
+    private void updateHookStates() {
+        switch (state) {
+            case PERCENTAGE:
+                periodicIO.hookDemand = Util.clamp(periodicIO.hookDemand, -1.0, 1.0);
+                break;
+            case ANGLE:
+                periodicIO.hookDemand = Util.clamp(periodicIO.hookDemand, ClimberConstants.HOOK_MIN_ANGLE, ClimberConstants.HOOK_MAX_ANGLE);
+                break;
+            case LOCKED:
+                periodicIO.hookDemand = 0.0;
+                break;
         }
-    
-        public STATE getState() {
-            return this.state;
-        }
+    }
+
+
+
+    public void setState(STATE state) {
+        this.state = state;
+    }
+
+    public STATE getState() {
+        return this.state;
+    }
 }
