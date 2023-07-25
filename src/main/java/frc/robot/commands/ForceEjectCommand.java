ff4f34c9d2d6ae2c5ff50662487ebcf633eb47bb
KeseterG <zhiwan13579@126.com>
2023-07-25 17:50:42 +0800

    feat: more auto modes

--- src/main/java/frc/robot/commands/ForceEjectCommand.java
+++ src/main/java/frc/robot/commands/ForceEjectCommand.java
@@ -14,6 +14,7 @@ public class ForceEjectCommand extends FunctionalCommand {
                 },
                 (interrupted) -> {
                     indexer.setWantForceEject(false);
+                    indexer.setFastEject(true);
                     trigger.lock();
                 },
                 () -> false
